aa228b0be22bff75bacfcbf302039ea13650701d
Domas Lapinskas <domasx2@gmail.com>
2015-03-29 14:16:17 +0300

    fix dependencies

--- karma.conf.js
+++ karma.conf.js
@@ -12,7 +12,7 @@ module.exports = function(config) {
         'bower_components/angular-route/angular-route.js',
         'bower_components/angular-mocks/angular-mocks.js',
         'src/named-route-module.js',
-        'src/**/*.js'
+        'src/**/*.js',
         'test/**/*.js'
     ],
 
